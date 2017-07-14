ae5c658cec53fa6a7988645dd87bcb1eb1186a6e
Eric Windmill <ericwindmill@gmail.com>
2017-07-14 15:39:41 -0700

    Render Concerts List

--- src/scripts/components/Concerts.jsx
+++ src/scripts/components/Concerts.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
 
 class Concerts extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      key: 0
+    }
+  }
 
 
-
-  componentWillReceiveProps (nextProps) {
-
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.concerts !== this.props.concerts) {
+      
+    }
   }
 
   render() {
-    console.log(this.props)
+    var concertsList = Object.entries(this.props.concerts).map(([id, concert]) => {
+          console.log(concert)
+                          return <li key={id}>{concert.artist[0]}</li>
+                       })
     return (
       <div>
-        {this.props.concerts.forEach(concert => {
-          <li>concert.artist[0]</li>
-        })}
+        <ul> 
+          { concertsList }
+        </ul>
       </div>
     )
   }
